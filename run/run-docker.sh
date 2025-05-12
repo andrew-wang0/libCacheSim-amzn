3db120b7b79df0e133e4c1a82f2bf365cadda9b1
Andrew Wang <andwwang@amazon.com>
2025-05-12 17:55:56 +0000

    chore: update run

--- run/run-docker.sh
+++ run/run-docker.sh
@@ -7,18 +7,24 @@ csv_file="$1"
 sizes=$(seq -s, 1000 1000 50000)
 algos=(lru lfu fifo arc cacheus sieve clock lirs s3fifo qdlp lecar lhd hyperbolic gdsf)
 
+cd ..
 docker build -t cache-sim .
+build_status=$?
 
-for algo in "${algos[@]}"; do
-  docker run --name cache-sim \
-    -v "$csv_file":/data.csv \
-    cache-sim /data.csv csv -t "time-col=1,obj-id-col=2" \
-    "$algo" \
-    "$sizes" \
-    --ignore-obj-size=1 \
-    --output=/output.csv
+if [ $build_status -eq 0 ]; then
+    cd run
 
-  timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-  docker cp cache-sim:/output.csv "./results/output-${algo}-${timestamp}.csv"
-  docker rm cache-sim --force
-done
+    for algo in "${algos[@]}"; do
+        docker run --name cache-sim \
+            -v "$csv_file":/data.csv \
+            cache-sim /data.csv csv -t "time-col=1,obj-id-col=2" \
+            "$algo" \
+            "$sizes" \
+            --ignore-obj-size=1 \
+            --output=/output.csv
+
+        timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+        docker cp cache-sim:/output.csv "./results/output-${algo}-${timestamp}.csv"
+        docker rm cache-sim --force
+    done
+fi
--- run/run.sh
+++ run/run.sh
@@ -11,12 +11,4 @@ csv_file="$1"
   lru,lfu,fifo,arc,cacheus,sieve,clock,lirs,s3fifo,qdlp,lecar,lhd,hyperbolic,gdsf \
   $(seq -s, 1000 500 50000) \
   --ignore-obj-size=1 \
-  --output=../out
-
-#docker run --rm --name cache-sim \
-#  -v "$csv_file":/data.csv \
-#  cache-sim /data.csv csv -t "time-col=1, obj-id-col=2" \
-#  lru,lfu,fifo,arc,cacheus,sieve,clock,lirs,s3fifo,qdlp,lecar,lhd,hyperbolic,gdsf \
-#  $(seq -s, 1000 500 50000) \
-#  --ignore-obj-size=1 \
-#  --output=/out
+  --output=../output.csv
